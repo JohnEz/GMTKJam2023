7c5d2b935779d0739254cc8edfc2b787611447e4
unknown <andrew_nomie@msn.com>
2023-07-08 08:42:05 +0100

    Add health bar

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,5 +1,34 @@
+using System;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour {
     public float MoveSpeed;
+
+    [SerializeField]
+    private float _maxHealth;
+
+    [HideInInspector]
+    public float MaxHealth { get => _maxHealth; set => SetMaxHealth(value); }
+
+    private float _currentHealth;
+
+    [HideInInspector]
+    public float CurrentHealth { get => _currentHealth; set => SetCurrentHealth(value); }
+
+    public Action OnHealthChanged;
+
+    private void Awake() {
+    }
+
+    private void SetCurrentHealth(float value) {
+        _currentHealth = value;
+
+        OnHealthChanged?.Invoke();
+    }
+
+    private void SetMaxHealth(float value) {
+        _maxHealth = value;
+
+        OnHealthChanged?.Invoke();
+    }
 }
---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+    [SerializeField]
+    private Image healthBar;
+
+    [SerializeField]
+    private Image damageBar;
+
+    private const float DAMAGE_BAR_SHRINK_TIMER_MAX = 0.3f;
+    private const float BAR_SHRINK_SPEED = 5f;
+
+    private float damageBarShrinkTimer;
+    private float targetHealthPercent = 1;
+
+    [SerializeField]
+    private CharacterStats _myStats;
+
+    private void Awake() {
+        SetHp();
+    }
+
+    public void OnEnable() {
+        if (_myStats != null) {
+            _myStats.OnHealthChanged += SetHp;
+        }
+    }
+
+    public void OnDisable() {
+        if (_myStats != null) {
+            _myStats.OnHealthChanged -= SetHp;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (damageBarShrinkTimer < 0) {
+            UpdateBarToValue(damageBar, healthBar.fillAmount);
+        } else {
+            damageBarShrinkTimer -= Time.deltaTime;
+        }
+    }
+
+    public static void UpdateBarToValue(Image bar, float targetAmount) {
+        if (targetAmount == bar.fillAmount) {
+            return;
+        }
+        bar.fillAmount = Mathf.Lerp(bar.fillAmount, targetAmount, BAR_SHRINK_SPEED * Time.deltaTime);
+
+        float distance = Mathf.Abs(targetAmount - bar.fillAmount);
+        if (distance < 0.005f) {
+            bar.fillAmount = targetAmount;
+        }
+    }
+
+    public void SetHp() {
+        float health = _myStats.CurrentHealth;
+        float maxHealth = _myStats.MaxHealth;
+
+        targetHealthPercent = health / maxHealth;
+
+        healthBar.fillAmount = targetHealthPercent;
+        damageBar.fillAmount = Mathf.Max(damageBar.fillAmount, targetHealthPercent);
+
+        damageBarShrinkTimer = DAMAGE_BAR_SHRINK_TIMER_MAX;
+    }
+}
