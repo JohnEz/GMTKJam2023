25420e552853276d6aa2f7f997dcf831636110a9
unknown <andrew_nomie@msn.com>
2023-07-08 22:26:03 +0100

    Add character movement detection

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,11 +11,13 @@ public class CharacterController : MonoBehaviour {
     [SerializeField]
     private Transform _weaponAnchor;
 
-    private float MIN_RANGE = 3f;
+    private float MIN_RANGE = 4f;
     private float MAX_RANGE = 10f;
 
     private float _targetRange = 9f;
-    private float RANGE_TOLERANCE = 0.5f;
+    private float RANGE_TOLERANCE = 2f;
+
+    private bool isChangingDistance = false;
 
     private bool isCircleClockwise = true;
 
@@ -24,18 +26,23 @@ public class CharacterController : MonoBehaviour {
     private CharacterStats _myStats;
 
     [SerializeField]
+    private AiChecker _checks;
+
+    [SerializeField]
     private Animator _animator;
 
     private float RANDOM_ACTION_DELAY = 5f;
     private float RANDOM_ACTION_VARIANCE = .33f;
 
     private float randomActionTimer = 0f;
-    private float currentrandomActionDelay = 5f;
+    private float currentRandomActionDelay = 5f;
 
     private void Awake() {
         _movement = GetComponent<CharacterMovement>();
         _attacks = GetComponent<CharacterAttacks>();
         _myStats = GetComponent<CharacterStats>();
+
+        _checks?.Setup(target);
 
         _myStats.OnDeath += HandleDeath;
 
@@ -61,9 +68,9 @@ public class CharacterController : MonoBehaviour {
     private void CalculateRandomAction() {
         randomActionTimer += Time.deltaTime;
 
-        if (randomActionTimer >= currentrandomActionDelay) {
+        if (randomActionTimer >= currentRandomActionDelay) {
             randomActionTimer = 0f;
-            currentrandomActionDelay = Random.Range(RANDOM_ACTION_DELAY * (1 - RANDOM_ACTION_VARIANCE), RANDOM_ACTION_DELAY * (1 + RANDOM_ACTION_VARIANCE));
+            currentRandomActionDelay = Random.Range(RANDOM_ACTION_DELAY * (1 - RANDOM_ACTION_VARIANCE), RANDOM_ACTION_DELAY * (1 + RANDOM_ACTION_VARIANCE));
 
             // choose a random action
             float result = Random.value;
@@ -92,13 +99,36 @@ public class CharacterController : MonoBehaviour {
     private void MovementLogic() {
         Vector3 moveDirection = Vector3.zero;
 
-        if (Mathf.Abs(GetDistanceToTarget() - _targetRange) > RANGE_TOLERANCE) {
-            moveDirection = MoveTowardsTargetDistance();
-        } else {
-            moveDirection = RunAroundTarget();
+        float distanceToTarget = GetDistanceToTarget();
+
+        if (_checks.IsBackwardLava && distanceToTarget >= MIN_RANGE) {
+            _targetRange = distanceToTarget - .75f;
+            _movement.MoveDirection = MoveCloser();
+            return;
         }
 
-        _movement.MoveDirection = moveDirection;
+        if (_checks.IsLeftLava) {
+            isCircleClockwise = true;
+        }
+
+        if (_checks.IsRightLava) {
+            isCircleClockwise = false;
+        }
+
+        if (Mathf.Abs(distanceToTarget - _targetRange) > RANGE_TOLERANCE) {
+            isChangingDistance = true;
+        }
+
+        if (isChangingDistance) {
+            if (Mathf.Abs(distanceToTarget - _targetRange) > 0.05f) {
+                _movement.MoveDirection = MoveTowardsTargetDistance();
+                return;
+            } else {
+                isChangingDistance = false;
+            }
+        }
+
+        _movement.MoveDirection = RunAroundTarget();
     }
 
     private void AimingLogic() {
@@ -111,11 +141,21 @@ public class CharacterController : MonoBehaviour {
     }
 
     private Vector3 MoveTowardsTargetDistance() {
-        float moveCloser = GetDistanceToTarget() > _targetRange ? 1 : -1;
+        bool moveCloser = GetDistanceToTarget() > _targetRange;
 
+        return moveCloser ? MoveCloser() : MoveAway();
+    }
+
+    private Vector3 MoveCloser() {
         Vector3 directionToTarget = GetDirectionToTarget();
 
-        return directionToTarget * moveCloser;
+        return directionToTarget;
+    }
+
+    private Vector3 MoveAway() {
+        Vector3 directionToTarget = GetDirectionToTarget();
+
+        return directionToTarget * -1;
     }
 
     private Vector3 RunAroundTarget() {
