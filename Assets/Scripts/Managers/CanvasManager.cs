70e6ad5381b36a79b07ba6d7a8f438e0c0cd5422
Michael Orchard <michael.orchard@live.co.uk>
2023-07-09 10:59:35 +0100

    Add game over controls

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -1,25 +1,3 @@
-using TMPro;
-using UnityEngine;
-
 public class CanvasManager : Singleton<CanvasManager> {
-    [SerializeField]
-    private GameObject _gameOverScreen;
-
-    [SerializeField]
-    private TMP_Text _gameOverTitle;
-
-    [SerializeField]
-    private TMP_Text _gameOverMessage;
-
-    public void ShowGameOver(string title, string message) {
-        if (_gameOverTitle) {
-            _gameOverTitle.text = title;
-
-            if (_gameOverMessage) {
-                _gameOverMessage.text = message;
-            }
-        }
-
-        _gameOverScreen.SetActive(true);
-    }
+    public GameOverScreen GameOverScreen;
 }
