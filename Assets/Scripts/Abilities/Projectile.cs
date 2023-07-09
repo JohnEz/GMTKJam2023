36998193a1cbf60fcc0f32377df17b1ef7db4bc1
eplmason <elliotplmason@gmail.com>
2023-07-09 11:36:56 +0100

    Get a load of my sword

--- Assets/Scripts/Abilities/Projectile.cs
+++ Assets/Scripts/Abilities/Projectile.cs
@@ -35,6 +35,9 @@ public class Projectile : MonoBehaviour {
     [SerializeField]
     private int _damage = 10;
 
+    [SerializeField]
+    private bool _shiftPitch = false;
+
     public void Launch(Transform origin, Vector3 target) {
         _origin = origin;
 
@@ -45,7 +48,7 @@ public class Projectile : MonoBehaviour {
 
         transform.rotation = rotation;
 
-        AudioManager.Instance.PlaySound(_onSpawnSFX, transform.position);
+        AudioManager.Instance.PlaySound(_onSpawnSFX, transform.position, _shiftPitch);
         AudioManager.Instance.PlaySound(_onloopSFX, transform);
     }
 
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -10,22 +10,22 @@ public class AudioManager : Singleton<AudioManager> {
     [SerializeField]
     private AudioMixerGroup sfxMixer;
 
-    public void PlaySound(AudioClip clip, Vector3 worldPosition) {
+    public void PlaySound(AudioClip clip, Vector3 worldPosition, bool shiftPitch = false) {
         if (!clip) {
             return;
         }
 
         GameObject soundGameObject = CreateSoundGameObject(clip.name, worldPosition);
-        CreateAudioSource(clip, soundGameObject);
+        CreateAudioSource(clip, soundGameObject, shiftPitch);
     }
 
-    public void PlaySound(AudioClip clip, Transform parent) {
+    public void PlaySound(AudioClip clip, Transform parent, bool shiftPitch = false) {
         if (!clip) {
             return;
         }
 
         GameObject soundGameObject = CreateSoundGameObject(clip.name, parent);
-        CreateAudioSource(clip, soundGameObject);
+        CreateAudioSource(clip, soundGameObject, shiftPitch);
     }
 
     private GameObject CreateSoundGameObject(string clipName, Vector3 worldPosition) {
@@ -42,7 +42,7 @@ public class AudioManager : Singleton<AudioManager> {
         return soundGameObject;
     }
 
-    private void CreateAudioSource(AudioClip clip, GameObject audioObject) {
+    private void CreateAudioSource(AudioClip clip, GameObject audioObject, bool shiftPitch = false) {
         AudioSource audioSource = audioObject.AddComponent<AudioSource>();
         audioSource.clip = clip;
         audioSource.maxDistance = 100f;
@@ -51,7 +51,7 @@ public class AudioManager : Singleton<AudioManager> {
         audioSource.dopplerLevel = 0f;
         audioSource.loop = false;
         audioSource.outputAudioMixerGroup = sfxMixer;
-
+        audioSource.pitch = shiftPitch ? Random.Range(0.8f, 1.5f) : 1;
         audioSource.Play();
 
         Destroy(audioObject, audioSource.clip.length);
