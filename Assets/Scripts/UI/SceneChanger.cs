fa3ffa5920f88080fe768db4cd24cd933ca6eca2
unknown <andrew_nomie@msn.com>
2023-07-14 15:22:42 +0100

    update main menu

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -1,9 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
-{
+public class SceneChanger : MonoBehaviour {
+    public Animator transition;
+
+    public float transitionTime = .75f;
+
     public void ChangeScene(string sceneName) {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    private IEnumerator LoadLevel(string sceneName) {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
         SceneManager.LoadScene(sceneName);
     }
 }
